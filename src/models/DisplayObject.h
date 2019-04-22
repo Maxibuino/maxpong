1fd4f765ef91171558bf840844ce5686531a1196
MaxMax <maxime.junchat@gmail.com>
2019-04-22 23:19:40 +0200

    dis

--- src/models/DisplayObject.h
+++ src/models/DisplayObject.h
@@ -3,6 +3,43 @@
 
 #include <Gamebuino-Meta.h>
 
+class DisplayObject
+{
+    protected:
+
+        // couleur par défaut de l'objet
+        static const Color DEFAUT_COLOR;
+
+        Color color; //Display Color
+
+        uint8_t w; //largeur
+        uint8_t h; //hauteur
+
+        int8_t x; //position horizontale
+        int8_t y; //position verticale
+
+        int8_t vx; //vitesse horizontale
+        int8_t vy; //vitesse verticale
+
+    public:
+
+    //Constructeur
+    DisplayObject(
+        uint8_t w,
+        uint8_t h,
+        int8_t x,
+        int8_t y,
+        int8_t vx = 0,
+        int8_t vy = 0,
+        Color   c = DISPLAY_DEFAULT_COLOR,
+
+    );
+
+    //Destructeur
+    ~DisplayObject();
+
+
+};
 
 
 #endif
